22aa7c8025797435cf3e3d12e64dd051743c5fff
Yozahim <kavan15@op.pl>
2020-02-12 16:21:31 +0100

    packages, picture, Main.vue ~mainly initializing project

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <div id="app">
+      <img height="150px" width="150px" alt="Vue logo" src="./assets/profilepicture.jpg">
+      <Main/>
+    </div>
+  <v-card
+    height="200px">
+    <v-footer padless>
+      <v-card
+        flat
+        tile
+        width="100%"
+        class="blue lighten-1 text-center">
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4"
+            icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-text>
+        
+      </v-card>
+    </v-footer>
+  </v-card>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      icons: ['instagram', 'github', 'mail']
+    }
+  },
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
---
+++ portfolio/src/components/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card
+  class="mainCardStyle"
+  raised>
+    <v-card-title>
+      <span outlined>Konrad Sobaniec</span>
+    </v-card-title>
+    <v-card-text>
+      
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'HelloWorld',
+    }
+  }
+}
+
+</script>
+<style scoped>
+.mainCardStyle{
+  min-width: 90%;
+  min-height: 90%;
+}
+</style>
